61442423dc7b93786e25ffb489dea362272ba17b
Michał Smolik <smolik.it@gmail.com>
2023-01-05 20:15:07 +0100

    add mock data to post module

--- src/models/post/post.controller.ts
+++ src/models/post/post.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Get, Param } from '@nestjs/common'
+import { Controller, Get, Param, UseGuards } from '@nestjs/common'
 import { PostService } from './post.service'
+import { AccessTokenGuard } from '../auth/strategies/access-token.guard'
+import { Post } from './types/post'
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
+  @UseGuards(AccessTokenGuard)
   @Get()
-  get(): string {
-    return this.postService.getPosts()
+  get(): Post[] {
+    return this.postService.getAll()
   }
 
+  @UseGuards(AccessTokenGuard)
   @Get(':id')
-  getOne(@Param('id') id: string): string {
-    return this.postService.getPost(id)
+  getOne(@Param('id') id: string): Post {
+    return this.postService.getOne(id)
   }
 }
--- src/models/post/post.service.ts
+++ src/models/post/post.service.ts
@@ -1,12 +1,29 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { Post } from './types/post'
+
+const posts: Post[] = [
+  { id: 1, title: 'Post 1' },
+  { id: 2, title: 'Post 2' },
+  { id: 3, title: 'Post 3' },
+  { id: 4, title: 'Post 4' },
+  { id: 5, title: 'Post 5' },
+  { id: 6, title: 'Post 6' },
+  { id: 7, title: 'Post 7' },
+  { id: 9, title: 'Post 8' },
+  { id: 8, title: 'Post 9' },
+]
 
 @Injectable()
 export class PostService {
-  getPosts(): string {
-    return 'posts'
+  getAll(): Post[] {
+    return posts
   }
 
-  getPost(id: string): string {
-    return `post-${id}`
+  getOne(id: string): Post {
+    const post = posts.find((post) => post.id === Number(id))
+
+    if (!post) throw new NotFoundException()
+
+    return post
   }
 }
